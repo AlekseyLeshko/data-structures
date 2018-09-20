118eb1792e41484ea79a88909d93dfb65b661e7e
AlekseyLeshko <alekseyleshko@gmail.com>
2018-09-21 01:19:03 +0300

    Rework addAtHead

--- list/linked-list/linked-list.js
+++ list/linked-list/linked-list.js
@@ -1,5 +1,21 @@
+const Node = function({ val, next }) {
+  this.value = val
+  this.next = next
+}
+
 const LinkedList = function() {
   this.head = null
+
+  this.addAtHead = (val) => {
+    if (this.head === null) {
+      this.head = new Node({ val })
+      return
+    }
+
+    const next = this.head
+    this.head = new Node({ val, next })
+  }
 }
+
 
 module.exports = LinkedList
--- list/linked-list/linked-list.test.js
+++ list/linked-list/linked-list.test.js
@@ -1,9 +1,27 @@
 const LinkedList = require('./linked-list')
 
 describe('linked-list', () => {
-  test('init', () => {
-    const list = new LinkedList()
+  let list
+  beforeEach(() => {
+    list = new LinkedList()
+  })
 
+  test('init', () => {
     expect(list.head).toBeNull()
+  })
+
+
+  test('add at head', () => {
+    expect(list.head).toBeNull()
+
+    list.addAtHead(3)
+
+    expect(list.head.value).toEqual(3)
+
+    list.addAtHead(2)
+
+    expect(list.head.value).toEqual(2)
+    expect(list.head.next.value).toEqual(3)
+    expect(list.head.next.next).toBeUndefined()
   })
 })
