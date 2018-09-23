c5e73aa0a963b35df6ca1af00c0dc156923d4a99
AlekseyLeshko <alekseyleshko@gmail.com>
2018-09-23 17:08:38 +0300

    Add addNode method

--- list/linked-list-2/linked-list.js
+++ list/linked-list-2/linked-list.js
@@ -1,10 +1,26 @@
-const Node = function({ val, next }) {
-  this.value = val
+const Node = function({ value, next }) {
+  this.value = value
   this.next = next
 }
 
 const LinkedList = function() {
   this.head = null
+
+  this.add = (value) => {
+    const newNode = new Node({ value })
+
+    if (!this.head) {
+      this.head = newNode
+      return
+    }
+
+    let currentNode = this.head
+    while (currentNode && currentNode.next) {
+      currentNode = currentNode.next
+    }
+
+    currentNode.next = newNode
+  }
 }
 
 module.exports = LinkedList
--- list/linked-list-2/linked-list.test.js
+++ list/linked-list-2/linked-list.test.js
@@ -9,4 +9,14 @@ describe('linked-list', () => {
   test('init', () => {
     expect(list.head).toBeNull()
   })
+
+  test('add node', () => {
+    list.add(3)
+    list.add(4)
+    list.add(5)
+
+    expect(list.head.value).toEqual(3)
+    expect(list.head.next.value).toEqual(4)
+    expect(list.head.next.next.value).toEqual(5)
+  })
 })
